c09fdce174873232f8a7ea537f00580a0e96bff6
27010-17-05 <27010-17-05@exchange.ad.afpanet>
2023-05-15 09:34:24 +0200

    web

--- gigant-projet/scripts/projet-js.js
+++ gigant-projet/scripts/projet-js.js
@@ -1,32 +1,28 @@
-let elt1 = document.querySelector("p");
-let elt2 = document.querySelector("command");
-let elt3 = document.getElementById("addinnerspace");
-let elt4 = document.getElementById("addouterspace");
-let elt5 = document.getElementById("toggle");
-let elt6 = document.getElementById("annote");
+/*manipuler le DOM
 
-let comment = "i = 42";
+// aller chercher un/des éléments dans le document*/
+let annote = document.getElementsByClassName('annote');
+console.log(annote);
+let span = document.getElementsByTagName('span')
+let command = document.querySelector("command");
+let comment = document.getElementsByTagName("comment");
+console.log(comment);
+let p = document.getElementsByTagName("p");
+console.log(p);
+let div = document.getElementsByTagName('div');
+console.log(div);
+/*Supprimer les caractères non-numériques au début de la chaîn*/
+let commentaire = "i = 42";
 function getNumber(chaine) {
-  /* Supprimer les caractères non-numériques au début de la chaîne*/
+
   let number = '';
   for (let i = 0; i < chaine.length; i++) {
     if (!isNaN(chaine[i])) {
       number += chaine[i];
     } else if (number !== '') {
       break;
-    }}
+    }
+  }
   return parseInt(number);
 }
-// Récupère tous les commentaires de la page
-const commentaires = document.querySelectorAll('.commentaire');
-
-// Ajoute un écouteur d'événements à chaque commentaire
-commentaires.forEach(commentaire => {
-  commentaire.addEventListener('mouseover', () => {
-    commentaire.style.backgroundColor = 'yellow'; // Ajoute un surlignage jaune
-  });
-
-  commentaire.addEventListener('mouseout', () => {
-    commentaire.style.backgroundColor = 'yellow'; // Supprime le surlignage
-  });
-});
+document.querySelector("span").style.backgroundColor = "yellow";
